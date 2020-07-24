896c3554602b54d43a4d884485849fd67c81e4c3
Guilherme Alves <guilherme-allves@hotmail.com>
2020-07-24 16:31:54 -0300

    Projeto 10 da pasta  FilipeFlop addicionado.

---
+++ Projetos FilipeFlop/10_sensor_de_luz/10_sensor_de_luz.ino
@@ -0,0 +1,22 @@
+// Projeto 10 - Sensor de luz ambiente
+
+int pinoLed = 12;
+int pinoLdr = A0;
+int valorLuz = 0;
+
+void setup() {
+  pinMode(pinoLed, OUTPUT);
+}
+
+void loop() {
+  valorLuz = analogRead(pinoLdr);
+
+  if (valorLuz < 750) {       // Sensibilidade do LDR => 0 a 1023.
+    digitalWrite(pinoLed, HIGH);
+  }
+  else {
+    digitalWrite(pinoLed, LOW);
+  }
+  delay(10);
+
+}
