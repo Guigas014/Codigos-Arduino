d3b3354bd685a8110fef60f26725a84daa07cbde
Guilherme Alves <guilherme-allves@hotmail.com>
2020-07-29 16:46:26 -0300

    Modificação no arquivo do-re-mi

--- Projetos FilipeFlop/11_do_re_mi/11_do_re_mi.ino
+++ Projetos FilipeFlop/11_do_re_mi/11_do_re_mi.ino
@@ -22,6 +22,7 @@ int melodia[] = {C, D, E, F, G, A, B, C8};
 int inc = 0;
 int testButton = 1;
 
+
 void setup() {
   pinMode(pinoBuzzer, OUTPUT);    //Configura o pino do buzzer como saída.
   pinMode(pinoButton, INPUT_PULLUP);
@@ -38,27 +39,48 @@ void loop() {
 //     inc = 0;
 //    }
 //  escalaDeC();
-  musica01();
+//  musica01();
+  caicaiBalao();
+//  alecrimDourado();
   }
   else {
     noTone(pinoBuzzer);
     testButton = 1;
   }
   
+  botaoTeste();
+
+//  if (digitalRead(pinoButton) == LOW and testButton == 1) {
+//    while (digitalRead(pinoButton) == LOW);
+//    delay(100);
+//    testButton = 0;
+//  }
+//  if (digitalRead(pinoButton) == LOW and testButton == 0) {
+//    while (digitalRead(pinoButton) == LOW);
+//    delay(100);
+//    testButton = 1;
+//  }
+
+}
+
+//-----------------------------------------------------------------
+int botaoTeste() {
   
   if (digitalRead(pinoButton) == LOW and testButton == 1) {
     while (digitalRead(pinoButton) == LOW);
     delay(100);
     testButton = 0;
+    
+    return testButton; 
   }
   if (digitalRead(pinoButton) == LOW and testButton == 0) {
     while (digitalRead(pinoButton) == LOW);
     delay(100);
     testButton = 1;
+
+    return testButton; 
   }
-
 }
-
 //-----------------------------------------------------------------
 // Modo mais simples de tocar as notas. (FUNÇÃO)
 void escalaDeC() {
@@ -105,6 +127,71 @@ void musica01() {
    testButton = 1;
    delay(10000);
 }
+//-------------------------------------------------------------
+void caicaiBalao() {
+  //Define o vetor da musica e o vetor do tempo.
+  int ode[] = {
+    P,G,G,F,E,G,G,F,E,
+    G,A,G,F,E,D,D,P,
+    D,E,F,D,E,F,D,E,F,
+    G,A,G,F,E,D,C
+  };
+  
+  int tempo[] = {
+    1,1.5,1.5,1,1,1.5,1.5,1,1,
+    1,1,1,1,1,1.5,1,1,
+    1,1,1,1,1,1,1,1,1.5,
+    1,1,1,1,1,1,1.5
+  };
+  
+  for (int i = 0; i<sizeof(ode)/2; i++) {
+    if (ode[i] != 0) {
+      tone(pinoBuzzer, ode[i]);
+    }
+    else {
+      noTone(pinoBuzzer);
+    }
+    delay(tempo[i]*500);
+    noTone(pinoBuzzer);
+  }
+  
+   noTone(pinoBuzzer);
+   testButton = 1;
+   delay(10000);
+  
+}
+//-------------------------------------------------------------
+void alecrimDourado() {
+  int ode[] = {
+    P,G,F,E,E,D,E,G,F,F,F,E,F,G,F,F,F,E,F,G,E,E,
+    G,F,E,E,D,E,G,F,F,F,E,F,G,F,F,F,E,F,G,E,E,P,
+    C,D,C,F,F,E,D,F,E,E,D,C,E,F,F,F,A,G,F,E,E,P,
+    C,D,C,F,F,E,D,F,E,E,D,C,E,F,F,F,A,G,F,E,G,F,E  
+  };
+  
+  int tempo[] = {
+    1,2,1,1,.250,.5,.5,1,1,.5,.5,.5,1,1,.5,.5,.5,1,1,2,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    .5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    .5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1.5,1,1,2  
+  };
+  
+  for (int i = 0; i<sizeof(ode)/2; i++) {
+    if (ode[i] != 0) {
+      tone(pinoBuzzer, ode[i]);
+    }
+    else {
+      noTone(pinoBuzzer);
+    }
+    delay(tempo[i]*500);
+    noTone(pinoBuzzer);
+  }
+  
+   noTone(pinoBuzzer);
+   testButton = 1;
+   delay(10000);
+}
+
 
 //-------------------------------------------------------------
 // Modo do site FILIPEFLOP de tocar as notas.
